abe9a3f486d8d1001e19ec34eb22f93527aab278
Kamolchai <hangx113@d.umn.edu>
2022-05-18 08:31:32 -0500

    Rendering classes and list dynamically

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,16 +3,35 @@ import React, { Component } from 'react'
 class Counter extends Component {
   state = { 
     count: 0,
-    imageURL: 'https://picsum.photos/200'
+    tags: ['tag1', 'tag2', 'tag3']
+    
   };
+
+  /*style ={
+    fontSize: 50,
+    fontWeight: 'bold'
+  };*/
   render() {
+    //best to use className to do formatiing
+
+    /* can get the classes with a variable or just call the method directly in the brackets
+    let classes = this.getBadgeClasses();*/
+
     return (
       <React.Fragment>
-        <img src ={this.state.imageURL} alt =""/>
-        <span>{this.formatCount()}</span> 
-        <button>Increment</button>
+        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span> 
+        <button className='btn btn-secoundary btn-sm'>Increment</button>
+        <ul>
+          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = 'badge m-2 badge-';
+    classes += (this.state.count === 0) ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount(){
