830623089184e86dd84ebd3fb3a676c7cfad033a
Kamolchai <hangx113@d.umn.edu>
2022-05-18 12:17:47 -0500

    Event handling

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,22 +5,26 @@ class Counter extends Component {
     count: 0
   };
 
-  //binding event handlers
+  //binding event handlers using contructor or = () => aka: arrow funciton
+
   /* constructor(){
     super();
      this.handleIncrement =this.handleIncrement.bind(this);
   } */
 
-  handleIncrement = ()=>{
-    console.log('Increment Clicked', this);
+  handleIncrement = (product)=>{
+    console.log(product);
+    this.setState({count: this.state.count +1});
   }
+
 
   render() {
     //handling events practice
     return (
       <React.Fragment>
         <span className= {this.getBadgeClasses()}>{this.formatCount()}</span> 
-        <button onClick={this.handleIncrement} className='btn btn-secoundary btn-sm'>Increment</button>
+        <button onClick={() => this.handleIncrement({id : 1})} 
+          className='btn btn-secoundary btn-sm'>Increment</button>
       </React.Fragment>
     );
   }
