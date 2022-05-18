6a155156e1e636c00bb91065fa2613c91dedf2b5
Kamolchai <hangx113@d.umn.edu>
2022-05-18 11:49:08 -0500

    Conditional rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,42 +3,29 @@ import React, { Component } from 'react'
 class Counter extends Component {
   state = { 
     count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: []
     
   };
 
-  /*style ={
-    fontSize: 50,
-    fontWeight: 'bold'
-  };*/
-  render() {
-    //best to use className to do formatiing
+  renderTags(){
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
 
-    /* can get the classes with a variable or just call the method directly in the brackets
-    let classes = this.getBadgeClasses();*/
+    return <ul>
+    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+  </ul>;
+  }
+
+  render() {
 
     return (
       <React.Fragment>
-        <span className= {this.getBadgeClasses()}>{this.formatCount()}</span> 
-        <button className='btn btn-secoundary btn-sm'>Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
 
-  getBadgeClasses() {
-    let classes = 'badge m-2 badge-';
-    classes += (this.state.count === 0) ? 'warning' : 'primary';
-    return classes;
-  }
-
-  formatCount(){
-    const { count } = this.state;
-    
-    return count === 0 ? 'Zero' : count;
-  }
+  
 }
 
 export default Counter
